7d193c025056acbcd39a8311947d039e34f16587
Aleksandr Vest <swest2462@gmail.com>
2016-05-23 23:54:35 +0300

    downcase text

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -3,6 +3,10 @@ class ImportsController < ApplicationController
   def create
     CsvImportService.new(params[:csv]).call
 
+    Description.all.each do |x|
+      x.update_attributes :value=> x.value.mb_chars.downcase
+    end
+
     render text: 'import ok'
   end
 end
